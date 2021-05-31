be25452f8bf4ef2a4a664f1000c0824b84534a3d
johnmicahmiguel <johnmicahmiguel@gmail.com>
2021-06-01 00:45:49 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+ 
+// mongoose.connect("mongodb://root:123409876@srv-captain--mongodb:27012/db", { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+// .then(() => {console.log('MongoDB successfully connected!')}, err => {console.log('Something went wrong!')});
+
+app.get('/', function (req, res) {
+    res.status(200).send({'message': "Welcome to NodeJS API"});
+})
+ 
+app.listen(process.env.PORT || 8080, ()=>{console.log("Server started!")});
+
